efcf5fdd30f5572772dd8b07016aa04e1c92676b
perrynaise <59591850+perrynaise@users.noreply.github.com>
2023-11-27 10:49:30 +1000

    added steps to only run database health checks when server actually has databases on it

--- scripts/exchange/exchange-node-maintenance.ps1
+++ scripts/exchange/exchange-node-maintenance.ps1
@@ -65,17 +65,20 @@ If ($Action -eq "suspend") {
         Set-MailboxServer $CurrentExchangeServer -DatabaseCopyAutoActivationPolicy Blocked
         $MoveResult = Move-ActiveMailboxDatabase -Server $CurrentExchangeServer
         
-        # Wait for all databases on $CurrentExchangeServer to be 'Mounted' elsewhere
-        Do
-        {
-        $ActiveCopies = Get-MailboxDatabaseCopyStatus -Server $CurrentExchangeServer | Where {$_.Status -eq "Mounted"}
-        } While ($ActiveCopies.count -ne 0)
-        
-        # Wait for all databases now 'Mounted' elsewhere to be in a 'Healthy' State
-        Do
-        {
-        $HealthyCopies = Get-MailboxDatabaseCopyStatus -Server $CurrentExchangeServer | Where {$_.Status -ne "Healthy"}
-        } While ($HealthyCopies.count -ne 0)
+        # If the Current Exchange Server has Databases Wait for Database Health
+        If ((Get-MailboxDatabase -Server $CurrentExchangeServer).count -ne 0) {
+            # Wait for all databases on $CurrentExchangeServer to be 'Mounted' elsewhere
+            Do
+            {
+            $ActiveCopies = Get-MailboxDatabaseCopyStatus -Server $CurrentExchangeServer | Where {$_.Status -eq "Mounted"}
+            } While ($ActiveCopies.count -ne 0)
+
+            # Wait for all databases now 'Mounted' elsewhere to be in a 'Healthy' State
+            Do
+            {
+            $HealthyCopies = Get-MailboxDatabaseCopyStatus -Server $CurrentExchangeServer | Where {$_.Status -ne "Healthy"}
+            } While ($HealthyCopies.count -ne 0)
+        }
         
         # Set all components of $CurrentExchangeServer to be 'InActive' for Maintenance
         Set-ServerComponentState $CurrentExchangeServer -Component ServerWideOffline -State InActive -Requester Maintenance
@@ -108,34 +111,37 @@ If ($Action -eq "suspend") {
         # Set Exchange Hub Transport Component to 'Active' after maintenance has been completed
         Set-ServerComponentState $CurrentExchangeServer -Component HubTransport -State Active -Requester Maintenance
         
-        # Activate one "Random" Database on $CurrentExchangeServer
-        $InActiveCopies = Get-MailboxDatabaseCopyStatus -Server $CurrentExchangeServer | Where {$_.Status -ne "Mounted"}
-        $MoveNum = (Get-Random -Maximum $InActiveCopies.count)
-        $MoveResult = Move-ActiveMailboxDatabase $InActiveCopies[$MoveNum].DatabaseName -ActivateOnServer $CurrentExchangeServer
-        
-        # Manual Buffer to give $MoveResult to stabilise - would like to later find a more accurate and dynamic way of monitoring this instead of a hardcoded timer.
-            # The reason this is even nessecary is that after activating a database it's queues are below the threshold immediately after the move, with the database in a healthy state.
-            # However there seems to be a spike within the next 30 seconds or so, that quickly disapates.
-            # One timer here, seems more efficient than two timers in both the Copy and Replay Queue While Loops.
-        Start-Sleep -Seconds 30
+        # If the Current Exchange Server has Databases Wait for Database Health
+        If ((Get-MailboxDatabase -Server $CurrentExchangeServer).count -ne 0) {
+            # Activate one "Random" Database on $CurrentExchangeServer
+            $InActiveCopies = Get-MailboxDatabaseCopyStatus -Server $CurrentExchangeServer | Where {$_.Status -ne "Mounted"}
+            $MoveNum = (Get-Random -Maximum $InActiveCopies.count)
+            $MoveResult = Move-ActiveMailboxDatabase $InActiveCopies[$MoveNum].DatabaseName -ActivateOnServer $CurrentExchangeServer
 
-        # Wait for all databases to be in either a 'Mounted' or 'Healthy' State
-        Do
-        {
-        $HealthyCopies = Get-MailboxDatabaseCopyStatus -Server $CurrentExchangeServer | Where {$_.Status -ne "Healthy" -and $_.Status -ne "Mounted"}
-        } While ($HealthyCopies.count -ne 0)
+            # Manual Buffer to give $MoveResult to stabilise - would like to later find a more accurate and dynamic way of monitoring this instead of a hardcoded timer.
+                # The reason this is even nessecary is that after activating a database it's queues are below the threshold immediately after the move, with the database in a healthy state.
+                # However there seems to be a spike within the next 60 seconds or so, that quickly disapates.
+                # One timer here, seems more efficient than two timers in both the Copy and Replay Queue While Loops.
+            Start-Sleep -Seconds 60
 
-        # Wait for all databases on $CurrentExchangeServer to have a Copy Queue of under $CopyQueueGoal
-        Do
-        {
-        $CopyQueues = Get-MailboxDatabaseCopyStatus -Server $CurrentExchangeServer | Where {$_.CopyQueueLength -gt $CopyQueueGoal}
-        } While ($CopyQueues.count -ne 0)
+            # Wait for all databases to be in either a 'Mounted' or 'Healthy' State
+            Do
+            {
+            $HealthyCopies = Get-MailboxDatabaseCopyStatus -Server $CurrentExchangeServer | Where {$_.Status -ne "Healthy" -and $_.Status -ne "Mounted"}
+            } While ($HealthyCopies.count -ne 0)
 
-        # Wait for all databases on $CurrentExchangeServer to have a Copy Queue of under $CopyQueueGoal
-        Do
-        {
-        $ReplayQueues = Get-MailboxDatabaseCopyStatus -Server $CurrentExchangeServer | Where {$_.ReplayQueueLength -gt $ReplayQueueGoal}
-        } While ($ReplayQueues.count -ne 0)
+            # Wait for all databases on $CurrentExchangeServer to have a Copy Queue of under $CopyQueueGoal
+            Do
+            {
+            $CopyQueues = Get-MailboxDatabaseCopyStatus -Server $CurrentExchangeServer | Where {$_.CopyQueueLength -gt $CopyQueueGoal}
+            } While ($CopyQueues.count -ne 0)
+
+            # Wait for all databases on $CurrentExchangeServer to have a Copy Queue of under $CopyQueueGoal
+            Do
+            {
+            $ReplayQueues = Get-MailboxDatabaseCopyStatus -Server $CurrentExchangeServer | Where {$_.ReplayQueueLength -gt $ReplayQueueGoal}
+            } While ($ReplayQueues.count -ne 0)
+        }
 
         # Output Result
         Write-Host "$($CurrentExchangeServer) has exited maintenance mode without error"
